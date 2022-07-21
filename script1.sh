8d901201777c1d9f4c72ff07ac8330d02f980787
EngineerAdnan-DEVOPS <83106571+AdnanMe-Devops@users.noreply.github.com>
2022-07-22 00:23:56 +0200

    Create script1.sh

---
+++ script1.sh
@@ -0,0 +1,52 @@
+whoami
+date
+echo $PATH
+pwd
+ls -la
+./gradlew build --info
+pipeline {
+  agent {
+    node {
+      label 'agent1'
+    }
+
+  }
+  stages {
+    stage('Clone') {
+      steps {
+        git(url: 'https://github.com/devops-webapp2', branch: 'master')
+      }
+    }
+    stage('Build') {
+      parallel {
+        stage('Build') {
+          steps {
+            sh '''whoami
+date
+echo $PATH
+pwd
+ls -la
+./gradlew build --info'''
+          }
+        }
+        stage('P1') {
+          steps {
+            sh '''date
+echo run parallel!!'''
+          }
+        }
+        stage('P2') {
+          steps {
+            sh '''date
+echo run parallel!!'''
+          }
+        }
+      }
+    }
+    stage('Publish') {
+      steps {
+        archiveArtifacts(artifacts: 'build/libs/*.war', fingerprint: true, onlyIfSuccessful: true)
+      }
+    }
+  }
+}
